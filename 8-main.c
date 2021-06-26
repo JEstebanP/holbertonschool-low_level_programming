a30b891b7b742a19f3836b51ee2c89edee8ada63
JEstebanP <kineropi@gmail.com>
2021-06-26 13:58:49 -0500

    5

---
+++ 8-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+    int i;
+    for ( i = 0; i < 10; i++)
+    {
+        printf("Hola\n");
+           }
+    
+    return 0;
+}
+
+void main2 (){      
+    
+}
