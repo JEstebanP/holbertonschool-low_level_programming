5bc85f1d99527ee794b4fdcfaabce4d4e3936c38
JEstebanP <kineropi@gmail.com>
2021-07-02 23:32:25 -0500

    1.1

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+int _putchar(char);
+char *_strcat(char *, char *);
+char *_strncat(char *, char *, int );
+
+#endif /* HOLBERTON_H */
